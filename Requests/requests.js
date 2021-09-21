02c395e44259012d80386ad22ebda9be21a67cfd
ddrichmond1998 <drichmondhealthexec@gmail.com>
2021-09-21 17:06:58 -0500

    Update requests.js

--- Requests/requests.js
+++ Requests/requests.js
@@ -4,9 +4,10 @@ const url = "http://localhost:5000/constellations";
 axios
   .get(url)
   .then((response) => {
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.data);
+    const result = response.data.filter((constellation) => {
+      return constellation.starsWithPlanets < 10;
+    });
+    console.log(result);
   })
   .catch((error) => {
     console.log(error.message);
